3e702a08b68376779fc854b99455b1d657383c64
havihaviplants <81484649+havihaviplants@users.noreply.github.com>
2024-01-13 23:57:47 +0900

    p140
    다양한 형태의 부동 소수점 출력하기

---
+++ p140.py
@@ -0,0 +1,14 @@
+output_a = "{:f}".format(52.273)
+output_b = "{:15f}".format(52.273)
+output_c = "{:+15f}".format(52.273)
+output_d = "{:+015f}".format(52.273)
+
+print(output_a)
+print(output_b)
+print(output_c)
+print(output_d)
+
+print(output_a)
+print(output_b)
+print(output_c)
+print(output_d)
