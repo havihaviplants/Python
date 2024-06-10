4df856218e1e9d0c917fec5d895bd2a6056c0aaf
havihaviplants <81484649+havihaviplants@users.noreply.github.com>
2024-06-10 23:57:06 +0900

    파이썬의 간단한 사칙연산 파일이다.
    혼자 공부하는 파이썬 P.105

---
+++ 사칙연산.py
@@ -0,0 +1,10 @@
+Python 3.12.2 (tags/v3.12.2:6abddd9, Feb  6 2024, 21:26:36) [MSC v.1937 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> print("5+7 = ", 5+7)
+5+7 =  12
+>>> print("5-7 =", 5-7)
+5-7 = -2
+>>> print("5*7=", 5*7)
+5*7= 35
+>>> print("5 / 7 =", 5/7)
+5 / 7 = 0.7142857142857143
